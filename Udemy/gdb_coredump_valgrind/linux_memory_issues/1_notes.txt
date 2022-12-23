1e4df7b2d14c82ea1977a3f84e5f32531b941bed
Harini-T <harini.hmt@gmail.com>
2022-12-23 06:05:30 +0000

    Updates in repository

--- Udemy/gdb_coredump_valgrind/linux_memory_issues/1_notes.txt
+++
@@ -1,3 +0,0 @@
-To get warning for this issue
-
-$ gcc 1.c -o 1 -Wall
--- Udemy/strace_ltrace/day11_strace/11/app.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main() 
-{
-	printf("hello world Learning Linux is very easy\n");
-	printf("bye world\n");
-	return 0;
-}
-
