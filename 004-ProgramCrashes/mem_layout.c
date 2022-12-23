f9db06fcf55ee73115821743baf1f24b3c2ad364
Harini Thiyagarajan <57031633+Harini-T@users.noreply.github.com>
2022-12-23 11:37:40 +0530

    memory layout

---
+++ 004-ProgramCrashes/mem_layout.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int q[200];
+int main(void){
+int i,n,*p;
+p=malloc(sizeof(int));
+scanf("%d",&n);
+for(i=0;i<200;i++)
+	q[i]=i;
+	
+printf("%x %x %x %x %x\n",main,q,p,&i,scanf);
+
+return 0;
+}
